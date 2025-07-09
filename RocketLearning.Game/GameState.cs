e3429b605798023b8937f1bde87587c8d9e3c262
Bouma <Christoferbouma@gmail.com>
2025-07-09 11:34:30 +0200

    Moved game logic and time logic from game state into GameViewModel

--- RocketLearning.Game/GameState.cs
+++ RocketLearning.Game/GameState.cs
@@ -1,40 +1,55 @@
 using System;
-using Avalonia.Threading;
 
 namespace RocketLearning.Game;
 
 public class GameState
 {
-    public Rocket Rocket { get; private set; } = new Rocket();
-    public Terrain Terrain { get; private set; } = new Terrain();
-    const int VelocityPunishment = 20;
-    const int AnglePunishment = 50;
-    const int TimePunishment = 3; 
+    public Rocket Rocket { get; private set; } = new();
+    public Terrain Terrain { get; private set; } = new();
+
     public double Score { get; private set; } = 0;
-    private double _timeScale  = 1;
-    public double Time { get; private set; } = 0.0;
+    public double Time { get; private set; } = 0;
+
     public bool Crashed { get; private set; } = false;
     public bool Landed { get; private set; } = false;
     public bool GameOver { get; private set; } = false;
+
     public event Action? OnStateChanged;
-    private readonly DispatcherTimer _timer;
-    private RocketInput _pendingInput = RocketInput.None;
 
+    private const int VelocityPunishment = 20;
+    private const int AnglePunishment = 50;
+    private const int TimePunishment = 3;
 
-
-    public GameState()
+    public void Tick(RocketInput input, double deltaTime)
     {
-        _timer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromMilliseconds(16/_timeScale) // ~60 FPS
-        };
-        _timer.Tick += (_, _) => TickRealTime();
-        _timer.Start();
+        if (GameOver)
+            return;
+
+        ApplyInput(input);
+        Rocket.Tick();
+        Time += deltaTime;
+
+        CheckLandingOrCrash();
+        ApplyConstantPunishment();
+
+        OnStateChanged?.Invoke();
     }
-    private void StepOnce(RocketInput input)
-    {
-        if (GameOver) return;
 
+    public void Reset()
+    {
+        Rocket = new Rocket();
+        Terrain = new Terrain();
+        Time = 0;
+        Score = 0;
+        Crashed = false;
+        Landed = false;
+        GameOver = false;
+
+        OnStateChanged?.Invoke();
+    }
+
+    private void ApplyInput(RocketInput input)
+    {
         switch (input)
         {
             case RocketInput.Left:
@@ -43,78 +58,45 @@ public class GameState
             case RocketInput.Right:
                 Rocket.RightMotor();
                 break;
-            case RocketInput.None:
-                break;
         }
-        Rocket.Tick();
-        Time += 1.0 / 60.0;
-
-        CheckLandingOrCrash();
-        ConstantPunishment();
-        
-        OnStateChanged?.Invoke();
-    }
-    public void SetInput(RocketInput input)
-    {
-        _pendingInput = input;
-    }
-    public void Reset()
-    {
-        Rocket = new Rocket();
-        Terrain = new Terrain();
-        Time = 0;
-        GameOver = false;
-        Landed = false;
-        Crashed = false;
-        Score = 0;
     }
 
-    public void TickRealTime()
+    private void ApplyConstantPunishment()
     {
-        if (GameOver) return;
-
-        StepOnce(_pendingInput);
-        _pendingInput = RocketInput.None;
+        double dx = Math.Abs(Rocket.PositionX - Terrain.PlatformX);
+        double dy = Math.Abs(Rocket.PositionY - Terrain.PlatformY);
+        double punishment = TimePunishment * dx * dy / 600000;
+        Score -= punishment;
     }
 
-    private void ConstantPunishment()
-    {
-        var punish = TimePunishment * (Math.Abs(Rocket.PositionX - Terrain.PlatformX) ) *
-            (Math.Abs(Rocket.PositionY - Terrain.PlatformY) ) / 600000;
-        Score -= punish;
-        Console.WriteLine($"Punishment: {punish}");
-    }
     private void CheckLandingOrCrash()
     {
-        var landing = Terrain.CheckLanding(Rocket);
-        
-        if (landing == RocketStates.Flying) return;
+        var result = Terrain.CheckLanding(Rocket);
+
+        if (result == RocketStates.Flying)
+            return;
+
         GameOver = true;
-        if (landing == RocketStates.LandedInbound)
+
+        if (result == RocketStates.LandedInbound)
         {
             Landed = true;
-            Crashed = false;
             Score += 1000;
         }
-        else if (landing == RocketStates.LandedOutbound)
+        else if (result == RocketStates.LandedOutbound)
         {
             Landed = true;
-            Crashed = false;
-            Score += 250 - Math.Abs(Rocket.PositionX-900);
+            Score += 250 - Math.Abs(Rocket.PositionX - 900);
         }
         else
         {
             Crashed = true;
-            Landed = false;
             Score -= 800;
         }
 
         Score -= Math.Abs(Rocket.VelocityX) * VelocityPunishment;
         Score -= Math.Abs(Rocket.VelocityY) * VelocityPunishment;
         Score -= Math.Abs(Rocket.Angle) * AnglePunishment;
-        Score = Math.Max(Score,10);
-        Console.WriteLine($"Final score:{Score}");
+        Score = Math.Max(Score, 10);
     }
-
-
 }
--- RocketLearning/ViewModels/GameViewModel.cs
+++ RocketLearning/ViewModels/GameViewModel.cs
@@ -3,64 +3,71 @@ using System.Windows.Input;
 using Avalonia.Input;
 using Avalonia.Threading;
 using RocketLearning.Game;
+using RocketLearning.Agent;
 
 namespace RocketLearning.ViewModels;
 
 public class GameViewModel : ViewModelBase
 {
-    private readonly GameState _state = new GameState();
-    public bool GameOver => _state.GameOver;
+    private readonly MainWindowViewModel _main;
+    private readonly GameState _state = new();
+    private readonly IAgent _agent;
+    private readonly DispatcherTimer _timer;
+
+    public ICommand ResetCommand { get; }
+    public ICommand BackToMenuCommand { get; }
+
+    public double RocketPositionX => _state.Rocket.PositionX - 80;
+    public double RocketPositionY => _state.Rocket.PositionY - 80;
+    public double RocketAngle => _state.Rocket.Angle;
     public double Score => _state.Score;
     public double BestScore => _main.SessionBestScore;
-
     public string FormattedTime => TimeSpan.FromSeconds(_state.Time).ToString(@"mm\:ss\.ff");
+
+    public bool GameOver => _state.GameOver;
+
     public double PlatformX => _state.Terrain.PlatformX;
     public double PlatformY => _state.Terrain.PlatformY;
     public double PlatformWidth => _state.Terrain.PlatformWidth;
     public double PlatformHeight => _state.Terrain.PlatformHeight;
-    public double LeftFlagX => _state.Terrain.PlatformX -55;
-    public double FlagY => _state.Terrain.PlatformY-30;
-    public double RightFlagX => _state.Terrain.PlatformX +PlatformWidth - 60;
-    public ICommand ResetCommand { get; }
-    public ICommand BackToMenuCommand { get; }
 
-    private readonly MainWindowViewModel _main;
+    public double LeftFlagX => PlatformX - 55;
+    public double RightFlagX => PlatformX + PlatformWidth - 60;
+    public double FlagY => PlatformY - 30;
 
-    public GameViewModel(MainWindowViewModel main)
+    public GameViewModel(MainWindowViewModel main, bool useNEAT = false)
     {
         _main = main;
-    
+        //_agent = useNEAT ? new NEATAgent(NeatLoader.LoadBestNetwork()) : new HumanAgent();
+        _agent = new HumanAgent();
         _state.OnStateChanged += () =>
         {
             OnPropertyChanged(nameof(RocketPositionX));
             OnPropertyChanged(nameof(RocketPositionY));
             OnPropertyChanged(nameof(RocketAngle));
-            OnPropertyChanged(nameof(GameOver));
             OnPropertyChanged(nameof(Score));
             OnPropertyChanged(nameof(FormattedTime));
+            OnPropertyChanged(nameof(GameOver));
             OnPropertyChanged(nameof(BestScore));
-            if (GameOver)
+            if (_state.GameOver)
                 _main.ReportScore(_state.Score);
         };
 
         ResetCommand = new RelayCommand(() => _state.Reset());
         BackToMenuCommand = new RelayCommand(() => _main.CurrentView = new MenuViewModel(_main));
-    }
 
-    public double RocketPositionX => _state.Rocket.PositionX - 80;
-    public double RocketPositionY => _state.Rocket.PositionY - 80;
-    public double RocketAngle => _state.Rocket.Angle;
+        _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(16) };
+        _timer.Tick += (_, _) =>
+        {
+            var input = _agent.Decide(_state);
+            _state.Tick(input, 1.0 / 60.0);
+        };
+        _timer.Start();
+    }
 
     public void OnKeyPressed(KeyEventArgs e)
     {
-        switch (e.Key)
-        {
-            case Key.Left:
-                _state.SetInput(RocketInput.Left);
-                break;
-            case Key.Right:
-                _state.SetInput(RocketInput.Right);
-                break;
-        }
+        if (_agent is HumanAgent human)
+            human.OnKeyPressed(e);
     }
 }
