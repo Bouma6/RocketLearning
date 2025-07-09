554ce9abe1a07fb6cbe9ce088186b91dd6e0a3fb
Bouma <Christoferbouma@gmail.com>
2025-07-09 17:14:51 +0200

    Implementation of NN

--- RocketLearning.ReinforcementLearning/NeuralNetwork.cs
+++ RocketLearning.ReinforcementLearning/NeuralNetwork.cs
@@ -7,7 +7,57 @@
 
 namespace RocketLearning.ReinforcementLearning;
 using System;
-public class NeuralNetwork
+public class NeuralNetwork(Func<double, double> activation)
 {
+    
+    public List<Node> Nodes = [];
+    public List<Connections> Connections = [];
+    public Func<double, double> ActivationFunction = activation;
 
+    public double[] FeedForward(double[] inputValues)
+    {
+        var nodeMap = Nodes.ToDictionary(n => n.Id);
+        int inputIndex = 0;
+        foreach (Node node in Nodes)
+        {
+            if (node.Type == NodeType.Input)
+            {
+                node.Weight = inputValues[inputIndex++];
+            }
+            else if (node.Type == NodeType.Bias)
+            {
+                node.Weight = 1;
+            }
+            else
+            {
+                node.Weight = 0;
+            }
+        }
+
+        var sorted = TopologicalSort();
+        foreach (var node in sorted)
+        {
+            foreach (var connection in Connections)
+            {
+                if (connection.Active && connection.FromId ==node.Id)
+                {
+                    //var target = Nodes.First(n => n.Id == connection.ToId);
+                    var target = nodeMap[connection.ToId];
+                    target.Weight += ActivationFunction(node.Weight* connection.Weight);
+                }
+            }
+        }
+        return Nodes
+            .Where(n => n.Type == NodeType.Output)
+            .Select(n => ActivationFunction(n.Weight))
+            .ToArray();
+    }
+
+
+    private List<Node> TopologicalSort()
+    {
+        //TO DO: do proper implementation when I will be less tired 
+        return Nodes.OrderBy(n => n.Type).ToList();
+    }
+    
 }
