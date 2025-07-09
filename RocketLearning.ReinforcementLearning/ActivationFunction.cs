2c6ee3e781293987f19a0ceac98e6c085bcd1f8f
Bouma <Christoferbouma@gmail.com>
2025-07-09 17:15:06 +0200

    Delegate activation functions

--- RocketLearning.ReinforcementLearning/ActivationFunction.cs
+++ RocketLearning.ReinforcementLearning/ActivationFunction.cs
@@ -1,5 +1,7 @@
 namespace RocketLearning.ReinforcementLearning;
-public class ActivationFunction
+public static class ActivationFunction
 {
-    
+    public static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
+    public static double Tanh(double x) => Math.Tanh(x);
+    public static double RelU(double x) => Math.Max(0.0, x);
 }
