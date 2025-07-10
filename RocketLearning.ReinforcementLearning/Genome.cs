138a0f31213b72f2eddeea75a94d5bbfc100f522
Bouma <Christoferbouma@gmail.com>
2025-07-10 19:42:04 +0200

    Added cloning method for genomes

--- RocketLearning.ReinforcementLearning/Genome.cs
+++ RocketLearning.ReinforcementLearning/Genome.cs
@@ -30,4 +30,40 @@ public class Genome
         };
         return network;
     }
+
+    public Genome Clone()
+    {
+        var clone = new Genome()
+        {
+            Nodes = Nodes.Select(n => new Node
+            {
+                Weight = 0,
+                Id = n.Id,
+                Type = n.Type
+            }).ToList(),
+
+            Connections = Connections.Select(c => new Connections
+            {
+                ToId = c.ToId,
+                FromId = c.FromId,
+                Weight = c.Weight,
+                InnovationNumber = c.InnovationNumber,
+                Active = c.Active
+            }).ToList(),
+            
+            Fitness = Fitness
+        };
+        return clone;
+    }
+
+    public void Mutate(Config config, Random random)
+    {
+        
+    }
+
+
+    public List<Genome> CrossOver(Config config, Random random)
+    {
+        
+    }
 }
