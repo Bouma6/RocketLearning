ec34ced671ce9edd678ad84bf4a7d954b4ffc593
Bouma <Christoferbouma@gmail.com>
2025-07-10 06:36:58 +0200

    Genome Implementation

--- RocketLearning.ReinforcementLearning/Genome.cs
+++ RocketLearning.ReinforcementLearning/Genome.cs
@@ -2,5 +2,32 @@ namespace RocketLearning.ReinforcementLearning;
 
 public class Genome
 {
+    public List<Node> Nodes = [];
+    public List<Connections> Connections = [];
+    public double Fitness = 0;
     
+    
+    //Build Neural Network from the genome, nodes and connections are copied 
+    public NeuralNetwork BuildNeuralNetwork(Func<double, double> activation)
+    {
+        var network = new NeuralNetwork(activation)
+        {
+            Nodes = Nodes.Select(n => new Node
+            {
+                Weight = 0,
+                Id = n.Id,
+                Type = n.Type
+            }).ToList(),
+
+            Connections = Connections.Select(c => new Connections
+            {
+                ToId = c.ToId,
+                FromId = c.FromId,
+                Weight = c.Weight,
+                InnovationNumber = c.InnovationNumber,
+                Active = c.Active
+            }).ToList()
+        };
+        return network;
+    }
 }
