fe2533a32f8441419a2420a3a227c8e50f3724ad
Bouma <Christoferbouma@gmail.com>
2025-07-11 16:52:05 +0200

    Initialization of population implemented - without random connections

--- RocketLearning.ReinforcementLearning/MasterTrainer.cs
+++ RocketLearning.ReinforcementLearning/MasterTrainer.cs
@@ -15,16 +15,7 @@ public class MasterTrainer
         _perCore = Config.PopulationSize / _cores;
         _evaluator = fitnessEvaluator;
 
-        for (int i = 0; i < cores;i++)
-        {
-            var currentPopulation = fullPopulation
-                .Skip(i * perCore)
-                .Take(perCore)
-                .Select(g =>g.Clone())
-                .ToList();
-            var trainer = new Trainer(currentPopulation, fitnessEvaluator, _config);
-            _trainers.Add(trainer);
-        }
+
     }
 
     public void Run(int generation = Config.NumberOfIterations)
@@ -46,9 +37,50 @@ public class MasterTrainer
     {
         
     }
-    private List<Genome> initializePopulation(int genomeCount)
+    private void InitializePopulation(int genomeCount)
     {
-        
+        var population = new List<Genome>();
+
+        for (int i = 0; i < genomeCount; i++)
+        {
+            var newGenome = new Genome();
+            //Input Nodes 
+            for (int j = 0; j < Config.InputSize; j++)
+            {
+                newGenome.Nodes.Add(new Node
+                {
+                    Id = j,
+                    Type = NodeType.Input,
+                });
+            }
+            //Bias Node 
+            newGenome.Nodes.Add(new Node
+            {
+                Id = Config.BiasNodeId,
+                Type = NodeType.Bias,
+            });
+            
+            //Output Nodes
+            for (int j = 0; j < Config.OutputSize; j++)
+            {
+                newGenome.Nodes.Add(new Node
+                {
+                    Id = Config.OutputNodesId +j,
+                    Type = NodeType.Output,
+                });
+            }
+            population.Add(newGenome);
+        }
+        for (int i = 0; i < _cores;i++)
+        {
+            var currentPopulation = population
+                .Skip(i * _perCore)
+                .Take(_perCore)
+                .Select(g =>g.Clone())
+                .ToList();
+            var trainer = new Trainer(currentPopulation, _evaluator, _config);
+            _trainers.Add(trainer);
+        }
     }
     
     
